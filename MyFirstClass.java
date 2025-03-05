214e3542dd4cd4ec750169cc254fb2a9fe1d3f62
Fahry Hang Lekiu <fahryhanglekiu456@gmail.com>
2025-03-05 22:13:40 +0700

    Add another class on main class

---
+++ MyFirstClass.java
@@ -0,0 +1,18 @@
+public class MyFirstClass{
+    public static void main(String[] args) {
+        System.out.println(GenerateMessage.GenerateOneMessage());
+        System.out.println(AnotherMessage.GenerateAnotherMessage());
+    }
+}
+
+class GenerateMessage{
+    static String GenerateOneMessage(){
+        return "Here is one message";
+    }
+}
+
+class AnotherMessage{
+    static String GenerateAnotherMessage(){
+        return "Here is another message";
+    }
+}
